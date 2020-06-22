a42f14d5019cd5be38c6f4da1a86619b38a793e0
Darius Kiuberis <dariuskiuberis@gmail.com>
2020-06-22 20:22:03 +0100

    add docs to type function

--- src/type.js
+++ src/type.js
@@ -1,3 +1,8 @@
+/**
+ * Function returns type of passed variable
+ *
+ * @param {*} prop {any} - prop
+ */
 const type = (prop) => {
   return Object.prototype.toString.call(prop).slice(8, -1).toLowerCase();
 };
